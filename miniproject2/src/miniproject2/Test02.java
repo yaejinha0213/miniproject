09cf0109034459e6142c5a995b94ff398bddb4d5
yaejinha <tttc0213@gmail.com>
2018-11-20 09:49:43 +0900

    test

--- miniproject2/src/miniproject2/Test02.java
+++
@@ -1,7 +0,0 @@
-package miniproject2;
-
-public class Test02 {
-	public static void main(String[] args) {
-		System.out.println("예진이");
-	}
-}
--- miniproject2/src/miniproject2/testnew.java
+++
@@ -1,7 +0,0 @@
-package miniproject2;
-
-public class testnew {
-	public static void main(String[] args) {
-		System.out.println("plzplze");
-	}
-}
